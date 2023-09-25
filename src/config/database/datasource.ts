2321a5b6a39ee126bc19b3842e61152ecf7b480d
Gabriel <gabriel.chaves.borges@gmail.com>
2023-09-25 22:44:05 +0900

    migrate from mysql to postgres

--- src/config/database/datasource.ts
+++ src/config/database/datasource.ts
@@ -2,17 +2,25 @@ import { PersonEntity } from "src/modules/person/person.entity";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'mariadb',
-  host: 'localhost',
-  username: 'root',
-  password: 'Elohim#3',
-  database: 'harvest_moon',
+  type: 'postgres',
+  host: 'dpg-ck80smfsasqs73ck817g-a.singapore-postgres.render.com',
+  username: 'gabe',
+  password: '29LShnEkS2kXWjpLFyoMBadXWnsChiis',
+  database: 'harvest_moon_db',
   entities: [
     PersonEntity,
   ],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
   synchronize: true,
   logging: true,
 }
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
+
+// PGPASSWORD=29LShnEkS2kXWjpLFyoMBadXWnsChiis psql -h dpg-ck80smfsasqs73ck817g-a.singapore-postgres.render.com -U gabe harvest_moon_db
--- src/modules/person/person.entity.ts
+++ src/modules/person/person.entity.ts
@@ -4,18 +4,18 @@ import { PersonInterface } from './person.interface';
 
 @Entity('person')
 export class PersonEntity implements PersonInterface {
-  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  @PrimaryGeneratedColumn({ type: 'smallint', name: 'id' })
   id: number;
 
-  @Column({ type: 'varchar', name: 'name', length: '20' })
+  @Column({ type: 'character varying', name: 'name', length: '20' })
   name: string;
 
   @Column({ type: 'enum', enum: Season, name: 'birth_season', nullable: true })
   birthSeason: Season;
 
-  @Column({ type: 'tinyint', name: 'birth_day', nullable: true })
+  @Column({ type: 'smallint', name: 'birth_day', nullable: true })
   birthDay: number;
 
-  @Column({ type: 'varchar', name: 'image', length: '2083', nullable: true })
+  @Column({ type: 'character varying', name: 'image', length: '2083', nullable: true })
   image: string;
 }
