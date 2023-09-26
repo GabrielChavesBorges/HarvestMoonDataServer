8f90916fc5b5d5e2fd2c39c0b66d22610e9b5164
Gabriel <gabriel.chaves.borges@gmail.com>
2023-09-26 20:54:13 +0900

    fix db leak

--- src/config/database/datasource.ts
+++ src/config/database/datasource.ts
@@ -1,11 +1,12 @@
 import { PersonEntity } from "src/modules/person/person.entity";
 import { DataSource, DataSourceOptions } from "typeorm";
+import "dotenv/config"
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: 'dpg-ck80smfsasqs73ck817g-a.singapore-postgres.render.com',
   username: 'gabe',
-  password: '29LShnEkS2kXWjpLFyoMBadXWnsChiis',
+  password: process.env.DB_PW,
   database: 'harvest_moon_db',
   entities: [
     PersonEntity,
